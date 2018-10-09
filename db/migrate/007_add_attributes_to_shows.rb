eca52403e574fadc44d083bb2431b655a405f5d7
Michael Roose <mjroose@gmail.com>
2018-10-09 23:59:36 +0000

    automatically backed up by learn

--- db/migrate/007_add_attributes_to_shows.rb
+++ db/migrate/007_add_attributes_to_shows.rb
@@ -1,9 +1,7 @@
 class AddAttributesToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows do |t|
-      t.string :day
-      t.string :season
-      t.string :genre
-    end
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 end
